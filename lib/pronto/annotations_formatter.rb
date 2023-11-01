aa7ee9c081f30d35ef974b5fcd5315ea9e2253bd
Emilio <emilio2hd@gmail.com>
2023-11-01 00:35:00 +0000

    Use the new registry method and remove the monkeypatch to include custom formatter.

--- lib/pronto/annotations_formatter.rb
+++ lib/pronto/annotations_formatter.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "pronto/formatter/formatter"
 require "pronto/formatter/base"
 
 require_relative "annotations_formatter/version"
@@ -7,6 +8,10 @@ require_relative "annotations_formatter/version"
 module Pronto
   module Formatter
     class AnnotationsFormatter < Pronto::Formatter::Base
+      def self.name
+        "annotations"
+      end
+
       # {
       #   "message": "Some message",
       #   "level": "warning",
@@ -46,28 +51,6 @@ module Pronto
       end
     end
   end
-
-  module AnnotationsFormatterMonkeypatch
-    ANNOTATION_OPTION = "annotations"
-    ORIGINAL_FORMATTERS = {}.freeze
-
-    # Get Pronto::Formatter::FORMATTERS constant and set a new one, adding the annotations leading to new formatter.
-    # As FORMATTERS is a frozen hash, it's required remove it and set a new one with the new option.
-    def self.apply_patch
-      return unless Pronto::Formatter.const_defined?(:FORMATTERS)
-      return if Pronto::Formatter::FORMATTERS.keys.include?(ANNOTATION_OPTION)
-
-      formatters_const = Pronto::Formatter.const_get(:FORMATTERS)
-      Pronto::Formatter.send(:remove_const, :FORMATTERS)
-
-      send(:remove_const, :ORIGINAL_FORMATTERS)
-      const_set(:ORIGINAL_FORMATTERS, formatters_const)
-      Pronto::Formatter.const_set(
-        :FORMATTERS,
-        formatters_const.merge(ANNOTATION_OPTION => Pronto::Formatter::AnnotationsFormatter).freeze
-      )
-    end
-
-    apply_patch
-  end
 end
+
+Pronto::Formatter.register(Pronto::Formatter::AnnotationsFormatter)
